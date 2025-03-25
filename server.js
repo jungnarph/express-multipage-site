a0272917f335a0253de8836e32292dab5c4365b9
Exulten <stem.danieljohnbucad@gmail.com>
2025-03-25 23:51:01 +0800

    Challenge Task

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require ('path');
+const fs = require ('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,20 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
+
+app.get('/api/posts', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'post.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: "Failed to load posts" });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 app.listen(PORT, () => {
